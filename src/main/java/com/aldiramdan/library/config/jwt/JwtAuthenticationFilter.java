d9ba5a5cad81676ed97b9731a74c9636a41b4cc5
aldiramdan <aldi.ramdan96@gmail.com>
2023-08-23 08:14:52 +0700

    add filter whitelist

--- src/main/java/com/aldiramdan/library/config/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/aldiramdan/library/config/jwt/JwtAuthenticationFilter.java
@@ -55,7 +55,10 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             final String username;
 
             if (Arrays.stream(SecurityConfiguration.whiteListedRoutes)
-                    .anyMatch(route -> antPathMatcher.match(route, request.getServletPath())) || Objects.isNull(authHeader) || !authHeader.startsWith("Bearer ")) {
+                    .anyMatch(route -> antPathMatcher.match(route, request.getServletPath())) ||
+                    Arrays.stream(SecurityConfiguration.getWhiteListedRoutes)
+                            .anyMatch(route -> antPathMatcher.match(route, request.getServletPath())) ||
+                    Objects.isNull(authHeader) || !authHeader.startsWith("Bearer ")) {
                 filterChain.doFilter(request, response);
                 return;
             }
@@ -79,7 +82,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                     SecurityContextHolder.getContext().setAuthentication(authToken);
                 }
             }
-            filterChain.doFilter(request, response);
         } catch (RuntimeException e) {
             response.setContentType(MediaType.APPLICATION_JSON_VALUE);
             response.setStatus(HttpStatus.UNAUTHORIZED.value());
@@ -91,5 +93,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             mapper.registerModule(new JavaTimeModule());
             mapper.writeValue(response.getOutputStream(), responseError);
         }
+        filterChain.doFilter(request, response);
     }
 }
