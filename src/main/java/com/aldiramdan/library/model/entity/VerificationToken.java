f7741d073898bd37886433d7900febf331da91c4
aldiramdan <aldi.ramdan96@gmail.com>
2023-08-30 08:13:28 +0700

    Add verification token

---
+++ src/main/java/com/aldiramdan/library/model/entity/VerificationToken.java
@@ -0,0 +1,29 @@
+package com.aldiramdan.library.model.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table
+public class VerificationToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String token;
+
+    @Column(nullable = false)
+    private LocalDateTime expiresAt;
+
+    private LocalDateTime confirmedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    public User user;
+}
