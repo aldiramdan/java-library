0280bab6eead980ed312318af02ad0f936dbae4a
aldiramdan <aldi.ramdan96@gmail.com>
2023-09-04 08:11:31 +0700

    Add interceptor

---
+++ src/main/java/com/aldiramdan/library/config/InterceptorConfig.java
@@ -0,0 +1,14 @@
+package com.aldiramdan.library.config;
+
+import com.aldiramdan.library.interceptor.LoggingInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class InterceptorConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoggingInterceptor());
+    }
+}
